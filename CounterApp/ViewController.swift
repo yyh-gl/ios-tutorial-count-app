09d53bbb5d42b19c63fad633aa47e013270e177f
honda-yusuke <honda-yusuke@dmm.com>
2019-05-23 13:17:07 +0900

    カウントアップアプリ完成

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countUpButton: UIButton!
+    @IBOutlet weak var CountDownButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        countLabel!.text = "0"
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func pushCountUpButton() {
+        guard let count = countLabel!.text.flatMap(Int.init) else
+        {
+            return
+        }
+        countLabel.text = "\(count + 1)"
+    }
+    
+    @IBAction func pushCountDownButton() {
+        guard let count = countLabel!.text.flatMap(Int.init) else
+        {
+            return
+        }
+        countLabel.text = "\(count - 1)"
+    }
 }
 
